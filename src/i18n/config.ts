32279529e7d0c0d1fbbcda0dd6567c5c97bbfa1a
frju365 <abld@abld.info>
2026-03-29 16:56:34 +0200

    [feat] add french translation

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,17 +1,18 @@
-export type LangCode = "en" | "hy" | "ru"
-type CountryCode = "us" | "am" | "ru"
+export type LangCode = "en" | "hy" | "ru" | "fr"
+type CountryCode = "us" | "am" | "ru" | "fr"
 type languageOption = { language: string; code: LangCode, countryCode: CountryCode };
-import {enUS,hy,ru, type Locale} from "date-fns/locale"
+import {enUS,hy,ru,fr, type Locale} from "date-fns/locale"
 
 export const languageOptions: languageOption[] = [
      { language: "English", code: "en", countryCode: "us"},
+     { language: "Français", code: "fr", countryCode: "fr"},
      { language: "Հայերեն", code: "hy", countryCode: "am"},
-     { language: "Русский", code: "ru", countryCode: "ru"},
+     { language: "Русский", code: "ru", countryCode: "ru"}
 ];
 
 export const dateFnsLanguages: Record<LangCode,Locale> = {
      en: enUS,
-     hy, ru
+     hy, ru, fr
 }
 
 export const suffixWhitelist: Set<LangCode> = new Set(["en"])
