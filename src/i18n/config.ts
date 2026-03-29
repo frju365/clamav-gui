04e7fc670bfd38f970a3379a76c98ec1c84feb74
frju365 <abld@abld.info>
2026-03-29 17:14:55 +0200

    merge: merge conflicts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,18 +1,22 @@
-export type LangCode = "en" | "hy" | "ru" | "fr"
-type CountryCode = "us" | "am" | "ru" | "fr"
+export type LangCode = "en" | "hy" | "ru" | "fr" | "pl"
+type CountryCode = "us" | "am" | "ru" | "fr" | "pl"
 type languageOption = { language: string; code: LangCode, countryCode: CountryCode };
-import {enUS,hy,ru,fr, type Locale} from "date-fns/locale"
+import * as DateFnsLocale from "date-fns/locale"
 
 export const languageOptions: languageOption[] = [
      { language: "English", code: "en", countryCode: "us"},
      { language: "Français", code: "fr", countryCode: "fr"},
      { language: "Հայերեն", code: "hy", countryCode: "am"},
-     { language: "Русский", code: "ru", countryCode: "ru"}
+     { language: "Русский", code: "ru", countryCode: "ru"},
+     { language: "Polski", code: "pl", countryCode: "pl" }
 ];
 
-export const dateFnsLanguages: Record<LangCode,Locale> = {
-     en: enUS,
-     hy, ru, fr
+export const dateFnsLanguages: Record<LangCode,DateFnsLocale.Locale> = {
+     en: DateFnsLocale.enUS,
+     hy: DateFnsLocale.hy,
+     ru: DateFnsLocale.ru,
+     pl: DateFnsLocale.pl,
+     fr: DateFnsLocale.fr
 }
 
 export const suffixWhitelist: Set<LangCode> = new Set(["en"])
